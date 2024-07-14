194f1ddd60c2166abba71da46f421c8ae2fd514c
amritdubey1600 <115389809+amritdubey1600@users.noreply.github.com>
2024-07-14 23:19:47 +0530

    Update index.js

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -28,7 +28,7 @@ const HeroSection = () => {
                             </Span>
                         </TextLoop>
                         <SubTitle>{Bio.description}</SubTitle>
-                        <ResumeButton href={Bio.resume} target='display'>Resume</ResumeButton>
+                        <ResumeButton href={Bio.resume} target='display'>Updated CV</ResumeButton>
                     </HeroLeftContainer>
 
                     <HeroRightContainer id="Right">
